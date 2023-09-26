da3cb95c51614854392a373f04851d517ea93058
Martin Michalec <michalecm23@spsit.local>
2023-09-26 08:56:17 +0200

    text

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int mojeCislo = 8;
+string mojeSlovo = "text    ";
+bool mojaBoolHodnota = true;
+DateTime dnesnyDatum = DateTime.Now;
+float myFloat = 8.88F;
+var myVar = myFloat + mojeCislo;
+
+
+Console.WriteLine(mojeCislo);
+Console.WriteLine(mojeSlovo);
+Console.WriteLine(mojaBoolHodnota);
+Console.WriteLine(dnesnyDatum);
+Console.WriteLine(myFloat);
+Console.WriteLine(myVar);
