4e99395591b463b59c6e7d422bd9d639106330e3
StempinA23 <StempinA23@Z00T6-13>
2023-09-26 09:48:38 +0200

    Stempin test

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -2,7 +2,7 @@
 string name = "Adam";
 Console.WriteLine(name);
 Console.WriteLine("Cesta k súboru je C:\\¯//(ツ)/¯\\https://texteditor.com/text-faces/");
-Console.WriteLine("Toto je \" "ano ano"\" reťazec.");
+Console.WriteLine("Toto je \"špeciálny\" reťazec.");
 Console.WriteLine(@"Zleteli orly z Tatry, tiahnu na podolia,
 ponad vysoké hory, ponad rovné polia;
 preleteli cez Dunaj, cez tú šíru vodu,
