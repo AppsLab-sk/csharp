670defed7ede1a7eebe33222c15aaaa56bbfdb39
AbafiM23 <AbafiM23@Z00T6-01>
2023-09-26 09:42:46 +0200

    Abafi

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+
+Console.WriteLine("Ahoj, svet!");
+string name = "ahoj Matúš";
+Console.WriteLine(name);
+Console.WriteLine("¯\\_(ツ)_/¯");
+Console.WriteLine(", ..,.,,.,.,,.,.,.,.,.,.,.,.");
+Console.WriteLine("Keď sa ráno zobudím na východ slnka,rieka pod oblokom tak krásne žblnká");
+
+    
