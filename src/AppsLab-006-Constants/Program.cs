efccb4c255e3a217570e7923eeb4d6ead3be089c
BojkoS23 <BojkoS23@Z00T6-07>
2023-09-26 09:09:47 +0200

    constants

--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string uvitaciaSprava = "Vitaj v tomto teste";
+const int maximalnyPocetZiakov = 28;
+const int najlepsiaZnamka = 1;
+const int najhorsiaZnamka = 5;
+
+
+Console.WriteLine(uvitaciaSprava);
+Console.WriteLine(maximalnyPocetZiakov);
+Console.WriteLine(najlepsiaZnamka);
+Console.WriteLine(najhorsiaZnamka);
+
