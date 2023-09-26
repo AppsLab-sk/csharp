9c8034e30c5ab27fe552060c20f510a40f9eb3ea
majaka23 <majaka23@Z00T6-04>
2023-09-26 09:11:40 +0200

    Maják test

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Adam!");
+int mojeCislo = 5;
+string mojeSlovo = "Ahoj, svet!";
+bool mojaBoolHodnota = true;
+DateTime dnesnyDatum = DateTime.Now;
+Console.WriteLine("mojeCislo");
+
+
