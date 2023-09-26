7ef5b5409ef8d5c55a7ea81a4e5c145b3c0bd0a8
BojkoS23 <BojkoS23@Z00T6-07>
2023-09-26 09:36:13 +0200

    viac konštánt

--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -2,10 +2,22 @@
 const int maximalnyPocetZiakov = 28;
 const int najlepsiaZnamka = 1;
 const int najhorsiaZnamka = 5;
+const bool hodnotaBool = false;
+const float floatHodnota = 32;
+DateTime presnyCas = DateTime.Now;
+var mojaHodnota = 35;
+
 
 
 Console.WriteLine(uvitaciaSprava);
 Console.WriteLine(maximalnyPocetZiakov);
 Console.WriteLine(najlepsiaZnamka);
 Console.WriteLine(najhorsiaZnamka);
+Console.WriteLine(hodnotaBool);
+Console.WriteLine(floatHodnota);
+Console.WriteLine(presnyCas);
+Console.WriteLine(mojaHodnota);
+
+
+
 
