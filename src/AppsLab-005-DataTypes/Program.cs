080226aea17eadfeeb8a95e839ff560af5065e4a
AbafiM23 <AbafiM23@Z00T6-01>
2023-09-28 08:07:46 +0200

    Abafi

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int mojecislo = 15;
+string slovo = "ahoj";
+bool boolhodnota = true;
+DateTime dneska = DateTime.Now;
+float f = 1.3f;
+var vr = 6;
+Console.WriteLine(mojecislo);
+Console.WriteLine(slovo);
+Console.WriteLine(boolhodnota);
+Console.WriteLine(dneska);
+Console.WriteLine(vr);
+Console.WriteLine(f);
+
+
