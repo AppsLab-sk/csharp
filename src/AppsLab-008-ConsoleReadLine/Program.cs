45b52526ba92b02479348de09ff8490772aa0e2e
majaka23 <majaka23@Z00T6-04>
2023-09-28 09:18:36 +0200

    :D

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
+Console.WriteLine("Ahoj, " + Adam);
 
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Máš " + 15 + " rokov.");
