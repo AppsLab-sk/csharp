b864739e71f0ceb6a92b8005145008980e03cba1
simikosik <kosiksimi5@gmail.com>
2023-09-28 08:50:22 +0200

    Kosik test

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int num = 10;
+string wrd = "mačka";
+var vr1 = "pes";
+bool bl = true;
+DateTime Dt = DateTime.Now;
+var vr2 = 12;
+float ft = 1.3f;
+Console.WriteLine(num + wrd + bl + Dt + vr1 + vr2 + ft);
