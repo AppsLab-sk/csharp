77f1e39459cad8e22d372c74619f5ec1e0512037
janicekj23 <janicekj23@Z00T6-04>
2023-10-03 09:12:58 +0200

    006

--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int MaxPoints = 100;
+const int MinAgeForDriversLicense = 17;
+const string WelcomeMessage = "Welcome in our game!";
+Console.WriteLine(MaxPoints + " Hp");
+Console.WriteLine(MinAgeForDriversLicense + " minimum age for driving license.");
+Console.WriteLine(WelcomeMessage);
