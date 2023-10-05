cbe328e2e196b9200da6d5453cbb8fff0d10c98c
MurinT23 <MurinT23@Z00T6-09>
2023-10-05 08:17:12 +0200

    Datatypes

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+int Cislo = 10;
+Console.WriteLine(Cislo);
+string Slovo = "Ahoj, svet!";
+Console.WriteLine(Slovo);
+bool BoolHodnota = true;
+Console.WriteLine(BoolHodnota);
+DateTime dnesnyDatum = DateTime.Now;
+Console.WriteLine(dnesnyDatum);
