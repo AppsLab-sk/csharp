b69a6fafcd4adf47de923242c5fbbb0d5c6f9ecc
KubinecS23 <KubinecS23@Z00T6-06>
2023-10-05 08:27:57 +0200

    ConsoleReadLine

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -5,4 +5,13 @@ Console.WriteLine("Ahoj, " + meno);
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Aha ! Máš " + vek + " rokov.");
+
+Console.WriteLine("Aké je tvoje obľúbené jedlo ? ");
+string? jedlo = Console.ReadLine();
+Console.WriteLine("Hmmmm " + jedlo + " by som si dal.");
+
+Console.WriteLine("Stlač klávesu: ");
+var key = Console.ReadKey();
+Console.WriteLine();
+Console.WriteLine($"Stlačili ste klávesu: {key.KeyChar}");
