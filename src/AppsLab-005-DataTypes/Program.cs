aac4756628141db767b035ea6d705d005f02c9dc
KminekD23 <KminekD23@Z00T6-05>
2023-10-05 08:30:00 +0200

    c

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,1 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int mojeCislo = 16;
+string mojeSlovo = "Ahoj Svet";
+bool mojaBoolHodnota = true;
+DateTime dnesnyDatum = DateTime.Now;
+Console.WriteLine(mojeCislo);
+Console.WriteLine(mojeSlovo);
+Console.WriteLine(dnesnyDatum);
--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+const int MaxPoints = 100;
+const int MinAgeForDrivingLicense = 17;
+const int WelcomeMessage = "Vitajte v našej hre!";
+Console.WriteLine(MaxPoints);
+Console.WriteLine(MinAgeForDrivingLicense);
+Console.WriteLine(WelcomeMessage);
