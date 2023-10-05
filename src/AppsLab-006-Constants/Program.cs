eb2ce780cc85afe20bc77bede5f51b3999d14b9c
MurinT23 <MurinT23@Z00T6-09>
2023-10-05 08:58:18 +0200

    6constanst

--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+const int MaxPoints = 100;
+Console.WriteLine(MaxPoints);
+const int MinAgeForDriversLicense = 17;
+Console.WriteLine(MinAgeForDriversLicense);
+const string WelcomeMessage = "Vitajte v našej krajine!";
+Console.WriteLine(WelcomeMessage);
