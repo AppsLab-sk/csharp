7f7308efe20a5e499b73ebfa8cbbefb08685523c
BojkoS23 <BojkoS23@Z00T6-07>
2023-10-10 09:47:27 +0200

    gamerules save

--- src/AppsLab-013-Conditions/GameRules.cs
+++ src/AppsLab-013-Conditions/GameRules.cs
@@ -1,4 +1,7 @@
-﻿namespace AppsLab_013_Conditions;
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AppsLab_013_Conditions;
 
 /// <summary>
 /// Represents the rules of a game.
@@ -13,7 +16,15 @@ public class GameRules
     /// <returns>True if the player can enter the game, false otherwise.</returns>
     public bool CanPlayerEnterGame(string playerName, int playerAge)
     {
-        throw new NotImplementedException();
+        if (playerAge >= 15)
+        {
+            if (playerName != "" && playerName != null)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        else { return false; }
     }
 
     /// <summary>
@@ -23,7 +34,12 @@ public class GameRules
     /// <returns>True if the game is over, false otherwise.</returns>
     public bool IsGameOver(int playerHealth)
     {
-        throw new NotImplementedException();
+        if (playerHealth <= 0)
+        {
+            return true;
+        }
+        else { return false; }
+
     }
 
     /// <summary>
@@ -34,6 +50,6 @@ public class GameRules
     /// <returns>True if the secret door should be opened, false otherwise.</returns>
     public bool ShouldOpenSecretDoor(bool hasKey, bool knowsPassword)
     {
-        throw new NotImplementedException();
+        
     }
 }
