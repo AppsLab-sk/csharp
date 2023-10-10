f3728bde8b25f27d5e5c733aefe67d59ee0422bf
MichalecM23 <MichalecM23@Z00T6-08>
2023-10-10 09:48:36 +0200

    Michalec save v2

--- src/AppsLab-014-Switch/MovieRating.cs
+++ src/AppsLab-014-Switch/MovieRating.cs
@@ -1,4 +1,6 @@
-﻿namespace AppsLab_014_Switch;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppsLab_014_Switch;
 
 /// <summary>
 /// Provides methods for working with movie ratings.
@@ -12,6 +14,16 @@ public class MovieRating
     /// <returns>A string describing the specified movie rating.</returns>
     public string GetRatingDescription(int rating)
     {
-        throw new NotImplementedException();
+        string numberDescription = rating switch
+        {
+            1 => "Veľmi zlý",
+            2 => "Zlý",
+            3 => "Priemerný",
+            4 => "Dobrý",
+            5 => "Výborný",
+            _ => "Neplatné hodnotenie",
+
+        }; 
+        return numberDescription;
     }
 }
