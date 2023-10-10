feba861e94b35f58594307d52f944ed1d4806c94
janicekj23 <janicekj23@Z00T6-04>
2023-10-10 09:58:00 +0200

    Commit 10.10

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// vypíše na konzolu reťazec "Hello World!"
+Console.WriteLine("Hello World!");
+
+/* Tento program je jednoduchá konzolová aplikácia v jazyku C#, ktorá pri spustení vypíše na konzolu reťazec "hello World!".
+Slúži na demonštráciu základného použitia metódy Console.WriteLine a práce s komentármi v jazyku C#. */
--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
+﻿using System.Diagnostics;
+
+string name = "Jakub";
+Console.WriteLine("Hello " + name);
+
+int age = 15;
+Console.WriteLine("Your age is " + age + """ years old""");
+
+DateTime dateTime = DateTime.Now;
+Console.WriteLine(dateTime.ToString());
+=======
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int mojeCIslo  = 25;
+string mojeSlovo = string.Empty;
+bool mojaBoolHodnota  = true;
+DateTime dateTimeHodnota = DateTime.Now;
+>>>>>>> 008
