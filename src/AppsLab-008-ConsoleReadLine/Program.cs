fa310bb9ef6a19aaed5dbe8c45f8f83ac37e0323
CubonT23 <CubonT23@Z00T6-10>
2023-10-12 08:57:07 +0200

    timaskove kodiki

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 Console.WriteLine("Ahoj, " + meno);
-
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
