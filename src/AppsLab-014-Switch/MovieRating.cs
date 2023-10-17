21262c16ae1e72eaf4e27d7dba607eb2cd6f3f8e
StempinA23 <StempinA23@Z00T6-13>
2023-10-17 08:42:58 +0200

    014

--- src/AppsLab-014-Switch/MovieRating.cs
+++ src/AppsLab-014-Switch/MovieRating.cs
@@ -10,8 +10,38 @@ public class MovieRating
     /// </summary>
     /// <param name="rating">The movie rating to get a description for.</param>
     /// <returns>A string describing the specified movie rating.</returns>
-    public string GetRatingDescription(int rating)
     {
-        throw new NotImplementedException();
+        public string GetRatingDescription(int rating)
+    {
+        string MovieRating = "";
+
+        switch (rating)
+        {
+            case 1:
+                MovieRating = "Veľmi zlý";
+                Console.WriteLine("Veľmi zlý");
+                break;
+            case 2:
+                MovieRating = "Zlý";
+                Console.WriteLine("Zlý");
+                break;
+            case 3:
+                MovieRating = "Priemerný";
+                Console.WriteLine("Priemerný");
+                break;
+            case 4:
+                MovieRating = "Dobrý";
+                Console.WriteLine("Dobrý");
+                break;
+            case 5:
+                MovieRating = "Výborný";
+                Console.WriteLine("Výborný");
+                break;
+            default:
+                MovieRating = "Neplatné hodnotenie";
+                Console.WriteLine("Neplatné hodnotenie");
+                break;
+        }
+        return MovieRating;
     }
 }
