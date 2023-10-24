2859c04c65a4a88e9170e99338667d8732e6d220
CubonT23 <CubonT23@Z00T6-10>
2023-10-24 08:05:29 +0200

    hotove

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,1 +1,5 @@
-﻿internal class Program
+﻿
+int mojeCislo = 12;
+string mojeSlovo = "Ahoj, svet!";
+bool mojaBoolHodnota = true;
+DateTime dnesnyDatum = DateTime.Now;
--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 Console.WriteLine("Ahoj, " + meno);
+
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
