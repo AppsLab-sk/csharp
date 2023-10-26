0d70a757515e2b781621c851ad4e761945d00dd7
BojkoS23 <BojkoS23@Z00T6-07>
2023-10-26 09:04:39 +0200

    listy a metody

--- src/AppsLab-017-List/Queue.cs
+++ src/AppsLab-017-List/Queue.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace AppsLab_017_List
 {
     /// <summary>
@@ -13,6 +15,7 @@ namespace AppsLab_017_List
         public Queue()
         {
             elements = new List<string>();
+            elements.Count();
         }
 
         /// <summary>
@@ -21,7 +24,9 @@ namespace AppsLab_017_List
         /// <param name="item">The item to add to the queue.</param>
         public void Enqueue(string item)
         {
-            throw new NotImplementedException();
+
+            elements.Add(item);
+
         }
 
         /// <summary>
@@ -30,7 +35,9 @@ namespace AppsLab_017_List
         /// <returns>The first item in the queue.</returns>
         public string Dequeue()
         {
-            throw new NotImplementedException();
+            string item = elements[0];
+            elements.RemoveAt(0);
+            return item;
         }
     }
 }
