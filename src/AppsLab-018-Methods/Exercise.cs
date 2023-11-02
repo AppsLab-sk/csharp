1d37f04d3f80f47808e6bd8f49217e62ebf80f19
StempinA23 <StempinA23@Z00T6-13>
2023-11-02 09:11:13 +0100

    018

--- src/AppsLab-018-Methods/Exercise.cs
+++ src/AppsLab-018-Methods/Exercise.cs
@@ -12,6 +12,18 @@ public class Exercise
     /// <returns>A string representation of the FizzBuzz game for the given number.</returns>
     public string FizzBuzz(int number)
     {
-        return "not implemented";
+        if (number % 4 == 0 && number % 7 == 0) //number is divadable by 4 & 7
+        {
+            return "FizzBuzz";
+        }
+        if (number % 4 == 0)
+        {
+            return "Fizz";
+        }
+        if (number % 7 == 0)
+        {
+            return "Buzz";
+        }
+        else return number.ToString();
     }
 }
