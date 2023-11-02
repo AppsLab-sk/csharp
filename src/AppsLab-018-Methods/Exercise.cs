61fbc96a8a35cb5e4fa91e86f3d82b93dbb53544
samuelbojko <bojkosam1@gmail.com>
2023-11-02 17:31:28 +0100

    commit test vo visual studio code

--- src/AppsLab-018-Methods/Exercise.cs
+++ src/AppsLab-018-Methods/Exercise.cs
@@ -12,6 +12,15 @@ public class Exercise
     /// <returns>A string representation of the FizzBuzz game for the given number.</returns>
     public string FizzBuzz(int number)
     {
-        return "not implemented";
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0 ) {
+            return "Buzz";
+        }
+        else {return number.ToString();}
     }
 }
