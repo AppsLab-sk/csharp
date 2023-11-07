e8f04b8037bc5f47bc6f24a86f0403726b3c8c3f
StempinA23 <StempinA23@Z00T6-13>
2023-11-07 10:06:19 +0100

    020

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AppsLab_020_OOP;
+using System;
+
+ namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Wizard mojwizard = new Wizard();
+          mojwizard.name = "Dakoty";
+          mojwizard.health = 130;
+            mojwizard.Attack = 200;
+            warior mojwarior = new warior();
+            mojwarior.name = "Minduri";
+            mojwarior.health = 300;
+            mojwarior.Attack = 80; 
+        }
+    }
+}
---
+++ src/AppsLab-020-OOP/warior.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_020_OOP
+{
+    internal class warior
+    {
+        public string name { get; set; }
+        public int health { get; set; }
+        public int Attack { get; set; }
+    }
+
+}
