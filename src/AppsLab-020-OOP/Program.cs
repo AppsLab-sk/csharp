089a0fed3b559e418225c2d76418a90654aa1e96
rtatigua <marcelkuljovsky273@gmail.com>
2023-11-07 10:00:14 +0100

    7.11

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AppsLab_020_OOP;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mage mojmag = new Mage();
+            mojmag.name = "Ferenci";
+            mojmag.Health = 250;
+            mojmag.Attack = 80;
+            Warrior mojwar = new Warrior();
+            mojwar.name = "Darius";
+            mojmag.Health = 300;
+            mojmag.Attack = 65;
+            Tank mojtan = new Tank();
+            mojtan.name = "Sion";
+            mojtan.Health = 500;
+            mojtan.Attack = 35;
+        }
+    }
+}
