1c834f39f13df4a3de4c6a9cd5fb236f113e7480
simikosik <kosiksimi5@gmail.com>
2023-11-09 09:18:56 +0100

    OOP 9.11.23

---
+++ src/AppsLab-020-OOP/Battle.cs
@@ -0,0 +1,14 @@
+﻿using AppsLab_020_OOP;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Battle
+    {
+        static void Main(string[] args)
+        {
+            
+
+    }
+}
+
--- src/AppsLab-020-OOP/Character.cs
+++ src/AppsLab-020-OOP/Character.cs
@@ -1,25 +1,18 @@
 ﻿
+using AppsLab_020_OOP;
+
 namespace AppsLab_020_OOP
 {
     public class Character
     {
         public string Name { get; set; }
         public int Health { get; set; }
-        public int Attack { get; set; }
-
-        public void Entrance()
+        public int AttackPower { get; set; }
+    
+        public void Attack() // (warrior health - mage attackpower)
         {
-            Console.WriteLine("The character makes an entrance");
+          
         }
-
-    }
-     public class Mage : Character
-    { 
-    public void Entrance()
-        {
-            Console.WriteLine("Enkudu shall defeat you!");
-        }
-    }
-
+        
 }
 
