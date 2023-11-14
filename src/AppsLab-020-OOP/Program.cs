b07912637d1736e87256fbb11ffaf05242d8695e
AbafiM23 <AbafiM23@Z00T6-01>
2023-11-14 08:33:21 +0100

    Abafi

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.PortableExecutable;
+
 Console.WriteLine("Hello, World!");
+
---
+++ src/AppsLab-020-OOP/hero.cs
@@ -0,0 +1,9 @@
+﻿using AppsLab_020_OOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_020_OOP
+  var hero  = new hero();
