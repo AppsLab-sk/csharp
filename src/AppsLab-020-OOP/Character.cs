3d49ff67d269ffb5afe031e9daa3ed2038ccb0b3
BojkoS23 <BojkoS23@Z00T6-07>
2023-11-14 08:46:42 +0100

    pokemon game

---
+++ src/AppsLab-020-OOP/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_020_OOP
+{
+    internal class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+       
+
+    }
+    
+}
