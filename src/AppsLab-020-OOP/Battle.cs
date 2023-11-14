32c103776eb082074aad5573f00c342fd038dc29
MinarikD23 <MinarikD23@Z00T6-11>
2023-11-14 09:46:19 +0100

    20

--- src/AppsLab-020-OOP/Battle.cs
+++ src/AppsLab-020-OOP/Battle.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 
 namespace AppsLab_020_OOP
 {
-         public class Enemy  
-        {
+         public class Character  
+         {
             public string Name { get; set; }
-            public string Age { get; set; }
-            public string Attack { get; set; }
-            public string Healt { get; set; }
+            public int Age { get; set; }
+            public int AttackPower { get; set; }
+            public int Health { get; set; }
+        
+          public int Attack()
+          {
+            return 0;
+
         }
 
-}
+        public void Defend()
+          {
 
-    
+          }
+          public void Heal()
+          {
+
+          }
+    }
+}
