8e9dbe36854058805babafcce2d866d9a38cec8d
KullaT23 <KullaT23@Z00T6-15>
2023-11-14 10:03:31 +0100

    najlepsia hra na svete. Onedlho battle pass pridam 16.11.2023

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AppsLab_020_OOP;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            wizard mojwizard = new wizard();
+            mojwizard.name = "Dakoty";
+            mojwizard.health = 400;
+            mojwizard.Attackpower = 100;
+            mojwizard.Mana = 100;
+
+            warrior mojwarior = new warrior();
+            mojwarior.name = "Ferenci";
+            mojwarior.health = 600;
+            mojwarior.Attackpower = 100;
+            
+            int round = 0;
+            while (mojwizard.StillAlive() && mojwarior.StillAlive())
+            {
+                mojwizard.Attack(mojwarior);
+                mojwarior.Attack(mojwizard);
+                Console.WriteLine((round++) + " " + mojwarior + " vs. " + mojwizard);
+            }
+            if (mojwizard.StillAlive())
+                Console.WriteLine("Wizard wins!");
+            else
+                Console.WriteLine("warior wins!");
+            if (mojwizard.StillAlive() && mojwarior.StillAlive())
+            {
+                Console.WriteLine("remiza :( ");
+            }
+
+        }
+    }
+}
---
+++ src/AppsLab-020-OOP/warrior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_020_OOP
+{
+    internal class warrior
+    {
+        public string name { get; set; }
+        public int health { get; set; }
+        public int Attackpower { get; set; }
+
+        public bool StillAlive()
+        {
+            return health > 0;
+        }
+
+        public override string ToString()
+        {
+            return name + "(" + health + ")";
+        }
+        public void Attack(wizard enemy)
+        {
+            Random random = new Random();
+            int attackpower = random.Next(Attackpower);
+            enemy.health-=attackpower;
+        }
+        public void Defend()
+        {
+
+        }
+        public void Heal()
+        {
+
+        }
+
+    }
+
+}
