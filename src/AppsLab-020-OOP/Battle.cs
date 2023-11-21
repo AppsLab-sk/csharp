838a305eb511fd6b665eb5593fdad9ae7c12bafc
janicekj23 <janicekj23@Z00T6-04>
2023-11-21 09:05:46 +0100

    020

---
+++ src/AppsLab-020-OOP/Battle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace AppsLab_020_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orc orc = new Orc();
+            Wizard wizard = new Wizard();
+
+            Battle.SimulateBattle(orc, wizard);
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class Battle
+    {
+        public static void SimulateBattle(Orc orc, Wizard wizard)
+        {
+            Console.WriteLine("Battle Start!");
+
+            while (orc.StillAlive() && wizard.StillAlive())
+            {
+                orc.Attack(wizard);
+
+                if (!wizard.StillAlive())
+                {
+                    Console.WriteLine(wizard.Name + " has been defeated. Orc wins!");
+                    break;
+                }
+
+                wizard.Attack(orc);
+
+                if (!orc.StillAlive())
+                {
+                    Console.WriteLine(orc.Name + " has been defeated. Wizard wins!");
+                    break;
+                }
+
+                wizard.IncrementRoundCounter();
+                wizard.GainMana();
+
+                orc.ResetAction();
+                wizard.ResetAction();
+
+                Console.WriteLine("Current Status:");
+                Console.WriteLine("Orc: " + orc);
+                Console.WriteLine("Wizard: " + wizard);
+                Console.WriteLine("----------------------");
+            }
+
+            Console.WriteLine("Battle End!");
+        }
+    }
+}
---
+++ src/AppsLab-020-OOP/Heroes.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace AppsLab_020_OOP
+{
+    public class Wizard
+    {
+        private static Random random = new Random();
+
+        public string Name { get; set; } = "Wizard";
+        public int Hp { get; set; } = 57;
+        public int Manna { get; set; } = 79;
+        public int Defense { get; set; } = 34;
+        public int AttackPower { get; set; }
+        private bool hasTakenAction = false;
+        private int roundsSinceLastDebuffRemoval = 0;
+
+        public Wizard()
+        {
+            if (Hp + Defense >= 50)
+            {
+                AttackPower = 60;
+            }
+            else
+            {
+                AttackPower = random.Next(30, 46);
+            }
+        }
+
+        public void Attack(Orc enemy)
+        {
+            if (!hasTakenAction)
+            {
+                if (Manna >= 4)
+                {
+                    Console.WriteLine("Wizard is attacking with magic: " + AttackPower);
+                    int damage = AttackPower - enemy.Defense;
+                    damage = Math.Max(0, damage); // Ensure damage is not negative
+                    enemy.Hp -= damage;
+
+                    Console.WriteLine("Enemy's Health after attack is: " + enemy.Hp);
+
+                    if (enemy.Hp <= 0)
+                    {
+                        Console.WriteLine("Enemy has died!");
+                    }
+
+                    Manna -= 4;
+                }
+                else
+                {
+                    Console.WriteLine("Not enough mana to perform the attack!");
+                }
+
+                hasTakenAction = true;
+            }
+            else
+            {
+                Console.WriteLine("Wizard has already taken an action this turn.");
+            }
+        }
+
+        public void Heal()
+        {
+            if (!hasTakenAction)
+            {
+                if (Manna >= 28)
+                {
+                    Console.WriteLine("Wizard is healing. Hp before healing: " + Hp);
+                    Hp += 40;
+                    Console.WriteLine("Hp after healing: " + Hp);
+
+                    Manna -= 28;
+                }
+                else
+                {
+                    Console.WriteLine("Not enough mana to perform the heal!");
+                }
+
+                hasTakenAction = true;
+            }
+            else
+            {
+                Console.WriteLine("Wizard has already taken an action this turn.");
+            }
+        }
+
+        public void IncrementRoundCounter()
+        {
+            roundsSinceLastDebuffRemoval++;
+        }
+
+        public void GainMana()
+        {
+            Manna += 9;
+        }
+
+        public void ResetAction()
+        {
+            hasTakenAction = false;
+        }
+
+        public bool StillAlive()
+        {
+            return Hp > 0;
+        }
+
+        public override string ToString()
+        {
+            return Name + "(" + Hp + ")";
+        }
+    }
+}
