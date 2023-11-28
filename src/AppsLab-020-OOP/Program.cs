2789bac372a4806f5a8200d00f641a8626431a3a
MinarikD23 <MinarikD23@Z00T6-11>
2023-11-28 10:09:35 +0100

    20

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -6,19 +6,44 @@ internal class Program
     {
         var save = File.ReadAllLines("Save/Saves.txt");
 
-        string[] wariorData = save[0].Split(";");
-        string[] enemyData = save[1].Split(";");
+        string teamA = save[0];
+        string teamB = save[6];
 
-        var warior = new Enemy(wariorData[0], Int32.Parse(wariorData[1]), Int32.Parse(wariorData[2]), Int32.Parse(wariorData[3]));
-        var enemy = new Enemy(enemyData[0], Int32.Parse(enemyData[1]), Int32.Parse(enemyData[2]), Int32.Parse(enemyData[3]));
+        List<Warior> Wariors = new List<Warior>();
+        List<Enemy> Enemies = new List<Enemy>();
 
+        for (int i = 1; i <= 5; i++)
+        {
+            string[] enemyData = save[i].Split(',');
+            var enemy = new Enemy(enemyData[0], Int32.Parse(enemyData[1]), Int32.Parse(enemyData[2]), 
+                Int32.Parse(enemyData[3]));
+            Enemies.Add(enemy);
+        }
 
+        for (int i = 7; i <= 11; i++)
+        {
+            string[] wariorData = save[i].Split(',');
+            var warior = new Warior(wariorData[0], Int32.Parse(wariorData[1]), 
+                Int32.Parse(wariorData[2]));
+            Wariors.Add(warior);
+        }
 
         int round = 0;
-        while (enemy.StillAlive() && warior.StillAlive())
+        while (Enemies.Count>0 && Wariors.Count>0)
         {
-            enemy.Attack(warior);
-            warior.Attack(enemy);
+            int EnemyIndex = r.Next(Enemies.Count-1);
+            int WariorIndex = r.Next(Wariors.Count - 1);
+
+            Enemies[EnemyIndex].Attack(Wariors[WariorIndex]);
+            Wariors[WariorIndex].Attack(Enemies[EnemyIndex]);
+            
+            if (Enemies[EnemyIndex].Health < 0)
+                Enemies.RemoveAt(EnemyIndex);
+            if (Wariors[WariorIndex].Health < 0)
+                Wariors.RemoveAt(WariorIndex);
+
+
+
             Console.WriteLine((round++) + " " + warior + " vs. " + enemy);
         } 
 
