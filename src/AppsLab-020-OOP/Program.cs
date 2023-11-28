93314dd53ac5ddd2d5110c3eac1653d0d6cb5d1b
rtatigua <marcelkuljovsky273@gmail.com>
2023-11-28 10:06:38 +0100

    28.11

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -14,14 +14,51 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
 
 
-            Mage mojmag = new Mage(wizardData[0], Int32.Parse(wizardData[1]), Int32.Parse(wizardData[2]), Int32.Parse(wizardData[3]));
-            Mage mojwar = new Mage(warriorData[0], Int32.Parse(warriorData[1]), Int32.Parse(warriorData[2]), Int32.Parse(warriorData[3]));
+            string[] saves = File.ReadAllLines(path: "saves/saves.txt");
+            string jednota = save[0];
+            string lidl = save[6];
 
-            int round = 0;
-            while (mojmag.StillAlive() && mojwar.StillAlive())
+            List<Warrior> lidlWar = new List<Warrior>();
+            List<Mage> jednotaMag = new List<Mage>();
+
+            for (int i = 1; i <= 5; i++)
             {
-                mojmag.Attack(mojwar);
-                mojwar.Attack(mojmag);
+                string[] mageData = save[i].Split(separator: ";");
+                var jednotamag1 = new Mage(wizardData[0], health: Int32.Parse(wizardData[1]),
+                   attackPower: Int32.Parse(wizardData[2]), mana: Int32.Parse(wizardData[3]));
+                jednotaMag.Add(jednotamag1);
+            }
+
+
+
+            for (int i = 7; i <= 11; i++)
+            {
+                string[] wariorData = save[0].Split(separator: ';');
+                var lidlwar1 = new Warrior(wariorData[0], health: Int32.Parse(wariorData[1]),
+                attackPower: Int32.Parse(wariorData[2]));
+                lidlWar.Add(lidlwar1);
+            }
+            Random r = new Random();
+            int round = 0;
+            while (jednotaMag.Count>0 && lidlWar.Count >0)
+            {
+                int jednotaindex = r.Next(jednotaMag.Count-1);
+                int lidlindex = r.Next(lidlWar.Count-1);
+               
+                
+                jednotaMag(jednotaindex).Attack(lidlWar[lidlindex]);
+                lidlWar(lidlindex).Attack(jednotaMag[jednotaindex]);
+
+                if (jednotaMag[jednotaindex] Health > 0;)
+                
+
+                
+                
+                
+               jednotaMag.Attack(lidlWar);
+
+
+
                 Console.WriteLine((round++) + " " + mojwar + "vs." + mojmag);
             }
             if (mojmag.StillAlive())
