77e520e3b77fb033a5689ec877bcd40dfa77dca0
rtatigua <marcelkuljovsky273@gmail.com>
2023-11-30 08:51:18 +0100

    30.11

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -8,13 +8,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
 
-            var save = File.ReadAllLines("Save/Saves.txt");
-            var warriorData = save[0].Split(';');
-            var wizardData = save[1].Split(";");
 
 
 
-            string[] saves = File.ReadAllLines(path: "saves/saves.txt");
+            string[] save = File.ReadAllLines(path: "save/saves.txt");
             string jednota = save[0];
             string lidl = save[6];
 
@@ -24,8 +21,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
             for (int i = 1; i <= 5; i++)
             {
                 string[] mageData = save[i].Split(separator: ";");
-                var jednotamag1 = new Mage(wizardData[0], health: Int32.Parse(wizardData[1]),
-                   attackPower: Int32.Parse(wizardData[2]), mana: Int32.Parse(wizardData[3]));
+                var jednotamag1 = new Mage(mageData[0], health: Int32.Parse(mageData[1]),
+                   attackPower: Int32.Parse(mageData[2]), mana: Int32.Parse(mageData[3]));
                 jednotaMag.Add(jednotamag1);
             }
 
@@ -33,40 +30,36 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             for (int i = 7; i <= 11; i++)
             {
-                string[] wariorData = save[0].Split(separator: ';');
+                string[] wariorData = save[i].Split(separator: ';');
                 var lidlwar1 = new Warrior(wariorData[0], health: Int32.Parse(wariorData[1]),
                 attackPower: Int32.Parse(wariorData[2]));
                 lidlWar.Add(lidlwar1);
             }
             Random r = new Random();
             int round = 0;
-            while (jednotaMag.Count>0 && lidlWar.Count >0)
+            while (jednotaMag.Count > 0 && lidlWar.Count > 0)
             {
-                int jednotaindex = r.Next(jednotaMag.Count-1);
-                int lidlindex = r.Next(lidlWar.Count-1);
-               
-                
-                jednotaMag(jednotaindex).Attack(lidlWar[lidlindex]);
-                lidlWar(lidlindex).Attack(jednotaMag[jednotaindex]);
-
-                if (jednotaMag[jednotaindex] Health > 0;)
-                
-
-                
-                
-                
-               jednotaMag.Attack(lidlWar);
+                int jednotaindex = r.Next(jednotaMag.Count - 1);
+                int lidlindex = r.Next(lidlWar.Count - 1);
 
 
+                jednotaMag[jednotaindex].Attack(lidlWar[lidlindex]);
+                lidlWar[lidlindex].Attack(jednotaMag[jednotaindex]);
 
-                Console.WriteLine((round++) + " " + mojwar + "vs." + mojmag);
+                
+
+                if (jednotaMag[jednotaindex].Health < 0)
+                    jednotaMag.RemoveAt(jednotaindex);
+                if (lidlWar[lidlindex].Health < 0)
+                    lidlWar.RemoveAt(lidlindex);
+              
             }
-            if (mojmag.StillAlive())
-                Console.WriteLine("Miloš wins");
-            if (mojwar.StillAlive())
-                Console.WriteLine("Gregor wins");
-            if (!mojmag.StillAlive() && !mojwar.StillAlive())
-                Console.WriteLine("Remíza");
+            if (lidlWar.Count > 0)
+                Console.WriteLine("Lidl je tá správna voľba");
+            if (jednotaMag.Count > 0)
+                Console.WriteLine("V jednote je sila");
+            if (lidlWar.Count == 0 && jednotaMag.Count == 0)
+                Console.WriteLine("Kaufland on top");
         }
     }
 }
