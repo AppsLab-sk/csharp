8c8eccb6395de168fcf2a602dcf9fcf1bdf9d046
CubonT23 <CubonT23@Z00T6-10>
2023-11-30 09:16:09 +0100

    20 hotová

--- src/AppsLab-020-OOP/Program.cs
+++ src/AppsLab-020-OOP/Program.cs
@@ -5,13 +5,13 @@ Console.WriteLine("Hello World!");
 
 var save = File.ReadAllLines("Save/Saves.txt");
 
-string teamA = save[0];
-string teamB = save[6];
+string warriorsTeamName = save[0];
+string magesTeamName = save[6];
 
 List<Warrior> warriors = new List<Warrior>();
 List<Mage> mages = new List<Mage>();
 
-for (int i = 1; i <= 5; i++)
+for (int i = 1; i <= 5; i++) 
 {
     string[] mageData = save[i].Split(";");
     var mage = new Warrior(mageData[0], Int32.Parse(mageData[1]), Int32.Parse(mageData[2]));
@@ -20,16 +20,16 @@ for (int i = 1; i <= 5; i++)
 
 for (int i = 7; i <= 11; i++)
 {
-    string[] warriorData = save[0].Split(';');
-    var mage = new Mage(save[0].Split(';')[0], Int32.Parse(save[0].Split(';')[1]), Int32.Parse(save[0].Split(';')[2]), Int32.Parse(save[0].Split(';')[3]));
+    string[] warriorData = save[i].Split(';');
+    var mage = new Mage(warriorData[0], Int32.Parse(warriorData[1]), Int32.Parse(warriorData[2]), Int32.Parse(warriorData[3]));
     mages.Add(mage);
 }
 Random r = new Random();
 int round = 0;
-while (mages.Count>0 && warriors.Count>0)
+while (mages.Count > 0 && warriors.Count > 0) 
 {
     int mageIndex = r.Next(mages.Count-1);
-    int warriorIndex = r.Next(warriors.Count - 1);
+    int warriorIndex = r.Next(warriors.Count - 1); 
 
 
     mages[mageIndex].Attack(warriors[warriorIndex]);
@@ -39,19 +39,17 @@ while (mages.Count>0 && warriors.Count>0)
         mages.RemoveAt(mageIndex);
     if (warriors[warriorIndex].Health < 0)
         warriors.RemoveAt(warriorIndex);
-
-    Console.WriteLine(round++ + " " + warrior + " vs " + mage);
 }
 
-if (warrior.StillALive())
+if (warriors.Count > 0)
 {
-    Console.WriteLine("vyhral bojovnik s menom: " + warrior.Name);
+    Console.WriteLine("vyhral team s menom: " + warriorsTeamName);
 }
-if (mage.StillALive())
+if (mages.Count > 0)
 {
-    Console.WriteLine("vyhral bojovnik s menom: " + mage.Name);
+    Console.WriteLine("vyhral team s menom: " + magesTeamName);
 }
-if (!mage.StillALive() && !warrior.StillALive())
+if (mages.Count==0 && warriors.Count==0)
 {
     Console.WriteLine("remiza :( ");
 }
