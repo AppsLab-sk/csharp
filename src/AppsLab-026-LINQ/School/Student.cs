330a25eafbc1f872d321d6cf8ae6bc938e071eb8
JanosikJ23 <JanosikJ23@Z00T6-12>
2023-12-05 09:58:42 +0100

    26

---
+++ src/AppsLab-026-LINQ/School/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_026_LINQ.School
+{
+    internal class Student : Person 
+    {
+        public string Class { get; set; }
+        public int Behaviour { get; set; }
+        public List<int> Grades { get; set; }
+
+        public Student(string @class, int behaviour, List<int> grades , int iD, string firstName, string lastName, string city, int age, double height)
+        {
+            Class = @class;
+            Behaviour = behaviour;
+            Grades = grades;
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            Age = age;
+            Height = height;
+        }
+    }
+}
