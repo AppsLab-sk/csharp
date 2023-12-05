3632e34849956d4d5bc6b632623783a18d6062be
simikosik <kosiksimi5@gmail.com>
2023-12-05 09:55:55 +0100

    5.12.23, 26

---
+++ src/AppsLab-026-LINQ/School/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_026_LINQ.School
+{
+    public class Student : Person
+    {
+        public string Class { get; set; }
+        public int Behavior { get; set; }
+
+        public List<int> Grades { get; set; } = new List<int>();
+
+        public Student(int id, string firstName, string lastName, int age, double height, string city, string @class, int behavior)
+        {
+            Class = @class;
+            Behavior = behavior;
+        }
+    }
+
+}
---
+++ src/AppsLab-026-LINQ/School/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_026_LINQ.School
+{
+    public class Teacher : Person
+    {
+        public int Salary {  get; set; }
+        public int Mood { get; set; }
+
+        public Teacher(int id, string firstName, string lastName, int age, double height, string city, int salary, int mood)
+        {
+            Salary = salary;
+            Mood = mood;
+        }
+    }
+}
