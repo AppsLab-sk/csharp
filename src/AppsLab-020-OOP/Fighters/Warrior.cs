3109fa2bf0879a4d3f7e1d0b07897b053fb24d74
BojkoS23 <BojkoS23@Z00T6-07>
2023-12-14 09:16:46 +0100

    save

---
+++ src/AppsLab-020-OOP/Fighters/Warrior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_020_OOP.Fighters
+{
+    internal class Warrior
+    {
+        public string Name { get; set; }
+        public int AttackPower { get; set; }
+        public int Health { get; set; }
+
+
+        public Warrior(string name, int attackPower, int health)
+        {
+            Name = name;
+            AttackPower = attackPower;
+            Health = health;
+        }
+
+        public bool StillAlive()
+        {
+            return Health > 0;
+        }
+
+        public void Attack(Wizard enemy)
+        {
+            enemy.Health -= AttackPower;
+        }
+
+        public override string? ToString()
+        {
+            return Name + "(" + Health + ")";
+        }
+
+        public void Heal()
+        {
+            Health += 25;
+        }
+    }
+}
---
+++ src/AppsLab-026-LINQ/School/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_026_LINQ.School
+{
+    public class Teacher : Person
+    {
+        public int Salary { get; set; }
+        public int Mood { get; set; }
+
+        public Teacher(int id, string name , int age , string lastname , string city ,double height , int salary , int mood ) : base (id , name , age , lastname , city ,height)
+        {
+            Salary = salary;
+            Mood = mood;
+        }
+    }
+}
