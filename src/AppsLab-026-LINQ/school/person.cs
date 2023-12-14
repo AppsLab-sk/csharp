6d3db719a98c8755939daa7d0cdd3e94d97b4f1f
AbafiM23 <AbafiM23@Z00T6-01>
2023-12-14 09:15:40 +0100

    Abafi

---
+++ src/AppsLab-026-LINQ/school/person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsLab_026_LINQ.school
+{
+    public class Person
+    {
+        int ID { get; set; }
+
+        string Name { get; set; }
+
+        string LastName { get; set; }
+
+        string City { get; set; }
+
+        int Age { get; set; }
+
+        double Height { get; set; }
+    }
+}
+
+
+
+
