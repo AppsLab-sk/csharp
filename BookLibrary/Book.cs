b55625535990e02670e8f0c1cada0a4330d065cb
KminekD23 <KminekD23@Z00T6-05>
2024-02-08 09:09:28 +0100

    Book 8.2

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public BookGenre Genre { get; set; }
+        public bool IsBorrowed { get; set; }
+        public DateTime BorrowedTime { get; set; }
+        public User BorrowedBy { get; set; }
+
+        public Book(int iD, string name, string Author, DateTime releaseDate, BookGenre genre)
+        {
+            ID= iD;
+            Name = name;
+            ReleaseDate = releaseDate;
+            Genre = genre;
+
+        }
+        public override string ToString()
+        {
+            if (IsBorrowed)
+                return $"{Name.PadRight(15, ' ')}{BorrowedBy.FullName.PadRight(15, ' ')}{BorrowedTime.ToString().PadRight(15, ' ')}";
+            else 
+                return $"{Name.PadRight(15,' ')}";
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+    public enum BookGenre
+    {
+        Scifi,
+        Comedy,
+        Cartoon,
+        Comix
+
+    }
+}
--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Hello, World!");
 const int MaxPoints = 100;
 const int MinAgeForDrivingLicense = 17;
-const int WelcomeMessage = "Vitajte v našej hre!";
+const string WelcomeMessage = "Vitajte v našej hre!";
 Console.WriteLine(MaxPoints);
 Console.WriteLine(MinAgeForDrivingLicense);
 Console.WriteLine(WelcomeMessage);
