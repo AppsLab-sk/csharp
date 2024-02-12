2c4fbac059d5dd74b1b13dc72993a9d603e5a343
martasskv5 <145648867+martasskv5@users.noreply.github.com>
2024-02-12 19:51:00 +0100

    save

--- BookLibraryGUI/MainWindow.xaml.cs
+++ BookLibraryGUI/MainWindow.xaml.cs
@@ -27,32 +27,32 @@ namespace BookLibraryGUI
         {
             InitializeComponent();
 
-            //List<Book> book = new List<Book>()
-            //{
-            //   new Book(0, "Ako naučiť Murína C#", "Michalec", new DateTime(2024, 02, 01), BookGenre.Scifi),
-            // new Book(1, "Ako naučiť Murína C# 2", "Michalec", new DateTime(2024, 02, 02), BookGenre.Scifi),
-            //new Book(2, "Ako naučiť Murína C# 3", "Michalec", new DateTime(2024, 02, 03), BookGenre.Scifi)
-            //};
+            /*List<Book> book = new List<Book>()
+            {
+               new Book(0, "Ako naučiť Murína C#", "Michalec", new DateTime(2024, 02, 01), BookGenre.Scifi),
+               new Book(1, "Ako naučiť Murína C# 2", "Michalec", new DateTime(2024, 02, 02), BookGenre.Scifi),
+                new Book(2, "Ako naučiť Murína C# 3", "Michalec", new DateTime(2024, 02, 03), BookGenre.Scifi)
+            };
 
-            //User murin = new User(12, "Murín", "Krasno pod Kysucou", new DateTime(2008, 05, 29), Gender.Male);
+            User murin = new User(12, "Murín", "Krasno pod Kysucou", new DateTime(2008, 05, 29), Gender.Male);
 
-            //book[2].IsBorrowed = true;
-            //book[2].BorrowedBy = murin;
-            //book[2].BorrowedTime = DateTime.Now;
+            book[2].IsBorrowed = true;
+            book[2].BorrowedBy = murin;
+            book[2].BorrowedTime = DateTime.Now;
 
-            //            for (int i = 0; i < book.Count; i++)
-            //          {
-            //            ListView.Items.Add(book[i].Name);
-            //          ListView2.Items.Add(book[i].Author);
-            //        ListView3.Items.Add(book[i].ReleaseDate);
-            //  }
-            Refresh();  
-           
+            for (int i = 0; i < book.Count; i++)
+            {
+                ListView.Items.Add(book[i].Name);
+                ListView2.Items.Add(book[i].Author);
+                ListView3.Items.Add(book[i].ReleaseDate);
+            } */
+            Refresh();
+
         }
         private void Refresh()
         {
             ListView.Items.Clear();
-            foreach (var book in LibraryKNM.Books.Where(x => !x.IsBorrowed).ToList()) 
+            foreach (var book in LibraryKNM.Books.Where(x => !x.IsBorrowed).ToList())
             {
                 ListView.Items.Add(book.ToString());
             }
